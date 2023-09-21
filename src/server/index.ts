b78837b25bf22038c3ecad0f5c53b24d233e93cf
Michael Mescua <mescua@outlook.com>
2023-09-21 18:39:30 -0500

    feature: server with express

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello from server');
+import express, { Express, Request, Response } from 'express';
+import { config } from './config';
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+	res.send(`<h1>Hello from Express with route: ${req.url}</h1>`);
+});
+
+app.listen(config.PORT, () => {
+	console.log(`Listening http://localhost:${config.PORT}`);
+});
